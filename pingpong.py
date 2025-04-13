5ec9316d61c39f56ba390e97e88be9949a0c6275
Geomancer <gvovvuur@gmail.com>
2025-04-13 19:46:01 +0500

    Add files via upload

---
+++ pingpong.py
@@ -0,0 +1,8 @@
+class GameSprite(sprite.Sprite): #Основной класс спрайта
+    def __init__(self, player_image, player_x, player_y, player_speed, size_x=65, size_y=65):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
